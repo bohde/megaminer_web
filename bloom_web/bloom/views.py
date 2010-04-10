cb1cbaaa5727334ae6b8b340b26144176ac53acc
Josh Bohde <josh.bohde@gmail.com>
2010-04-10 18:06:42 -0500

    Added view all for admins.

--- bloom_web/bloom/views.py
+++ bloom_web/bloom/views.py
@@ -2,10 +2,17 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from models import GameLog
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
 def index(request):
-    logs = GameLog.objects_with_tags(request.user)
+    logs = GameLog.my_objects(request.user)
     return render_to_response('bloom/list.html', {'logs':logs},
                               context_instance=RequestContext(request))  
+
+@permission_required('bloom.view_all')
+def all_logs(request):
+    logs = GameLog.objects_with_tags()
+    return render_to_response('bloom/list.html', {'logs':logs},
+                              context_instance=RequestContext(request))  
+
