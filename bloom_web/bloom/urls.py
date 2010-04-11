069d8559b49d6691e744b133a8fb159e89e99328
Josh Bohde <josh.bohde@gmail.com>
2010-04-10 23:09:28 -0500

    Added api

--- bloom_web/bloom/urls.py
+++ bloom_web/bloom/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from views import index, all_logs, top_n
+from views import index, all_logs, top_n, top_n_csv
 from models import GamePlayerInfo, GameLog
 import tagging
 
@@ -7,7 +7,8 @@ import tagging
 urlpatterns = patterns('',
     url(r'^$', index, name="index"),
     url(r'^all$', all_logs),
-    url(r'^top$', top_n, {'n':3}, name='top-competitors'),                       
+    url(r'^top$', top_n, {'n':3}, name='top-competitors'),
+    url(r'^api/top$', top_n_csv, {'n':3}, name='api-top-competitors'),                    
 )                       
 
 tagging.register(GamePlayerInfo)
